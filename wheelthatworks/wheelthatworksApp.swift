698c74f7d04b0da12d2c98cc22094500835861ed
EilafMohammad <elafmohammadcs@gmail.com>
2023-10-12 11:13:13 +0300

    Initial Commit

---
+++ wheelthatworks/wheelthatworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheelthatworksApp.swift
+//  wheelthatworks
+//
+//  Created by Eilaf Mohammad on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct wheelthatworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
